a3909960d32f490b79b8e23631aeb55c4896d483
Cristian Maglie <c.maglie@bug.st>
2016-10-22 15:39:46 +0200

    Removed termiosMask(..) and use the correct bitmask type
    This requires a bit of copying in darwin 386/amd64 but simplifies
    the overall design.

--- serial_darwin_386.go
+++ serial_darwin_386.go
@@ -6,6 +6,44 @@
 
 package serial // import "go.bug.st/serial.v1"
 
-func termiosMask(data uint) uint32 {
-	return uint32(data)
+import "syscall"
+
+// termios manipulation functions
+
+var baudrateMap = map[int]uint32{
+	0:      syscall.B9600, // Default to 9600
+	50:     syscall.B50,
+	75:     syscall.B75,
+	110:    syscall.B110,
+	134:    syscall.B134,
+	150:    syscall.B150,
+	200:    syscall.B200,
+	300:    syscall.B300,
+	600:    syscall.B600,
+	1200:   syscall.B1200,
+	1800:   syscall.B1800,
+	2400:   syscall.B2400,
+	4800:   syscall.B4800,
+	9600:   syscall.B9600,
+	19200:  syscall.B19200,
+	38400:  syscall.B38400,
+	57600:  syscall.B57600,
+	115200: syscall.B115200,
+	230400: syscall.B230400,
 }
+
+var databitsMap = map[int]uint32{
+	0: syscall.CS8, // Default to 8 bits
+	5: syscall.CS5,
+	6: syscall.CS6,
+	7: syscall.CS7,
+	8: syscall.CS8,
+}
+
+const tcCMSPAR uint32 = 0 // may be CMSPAR or PAREXT
+const tcIUCLC uint32 = 0
+
+const tcCCTS_OFLOW uint32 = 0x00010000
+const tcCRTS_IFLOW uint32 = 0x00020000
+
+const tcCRTSCTS uint32 = (tcCCTS_OFLOW | tcCRTS_IFLOW)
--- serial_darwin_amd64.go
+++ serial_darwin_amd64.go
@@ -6,8 +6,44 @@
 
 package serial // import "go.bug.st/serial.v1"
 
+import "syscall"
+
 // termios manipulation functions
 
-func termiosMask(data uint) uint64 {
-	return uint64(data)
+var baudrateMap = map[int]uint64{
+	0:      syscall.B9600, // Default to 9600
+	50:     syscall.B50,
+	75:     syscall.B75,
+	110:    syscall.B110,
+	134:    syscall.B134,
+	150:    syscall.B150,
+	200:    syscall.B200,
+	300:    syscall.B300,
+	600:    syscall.B600,
+	1200:   syscall.B1200,
+	1800:   syscall.B1800,
+	2400:   syscall.B2400,
+	4800:   syscall.B4800,
+	9600:   syscall.B9600,
+	19200:  syscall.B19200,
+	38400:  syscall.B38400,
+	57600:  syscall.B57600,
+	115200: syscall.B115200,
+	230400: syscall.B230400,
 }
+
+var databitsMap = map[int]uint64{
+	0: syscall.CS8, // Default to 8 bits
+	5: syscall.CS5,
+	6: syscall.CS6,
+	7: syscall.CS7,
+	8: syscall.CS8,
+}
+
+const tcCMSPAR uint64 = 0 // may be CMSPAR or PAREXT
+const tcIUCLC uint64 = 0
+
+const tcCCTS_OFLOW uint64 = 0x00010000
+const tcCRTS_IFLOW uint64 = 0x00020000
+
+const tcCRTSCTS uint64 = (tcCCTS_OFLOW | tcCRTS_IFLOW)
