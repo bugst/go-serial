a8b06090aeec719258b9c3f91c097aafd494cbf7
Maddie Zhan <maddie.zhan@cynovan.com>
2020-09-10 16:57:12 +0800

    Use golang.org/x/sys/unix provided ioctl functions

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/creack/goselect v0.1.1
 	github.com/stretchr/testify v1.4.0
-	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9
+	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009
 )
--- syscall_freebsd.go
+++
@@ -1,9 +0,0 @@
-//
-// Copyright 2014-2020 Cristian Maglie. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-//
-
-package serial
-
-//sys ioctl(fd int, req uint64, data uintptr) (err error)
