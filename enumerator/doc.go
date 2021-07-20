9cebdc6aa3036d196e0732194475c766b354ab89
Cristian Maglie <c.maglie@bug.st>
2021-07-20 12:54:16 +0200

    Small update to docs

--- enumerator/doc.go
+++ enumerator/doc.go
@@ -7,15 +7,9 @@
 /*
 Package enumerator is a golang cross-platform library for USB serial port discovery.
 
-WARNING: this library is still beta-testing code! please consider the library
-and the API as *unstable*. Beware that, even if at this point it's unlike to
-happen, the API may be subject to change until this notice is removed from
-the documentation.
-
 This library has been tested on Linux, Windows and Mac and uses specific OS
 services to enumerate USB PID/VID, in particular on MacOSX the use of cgo is
 required in order to access the IOKit Framework. This means that the library
-cannot be easily cross compiled for GOOS=darwing targets.
-
+cannot be easily cross compiled for darwin/* targets.
 */
 package enumerator
