1bf551e0252c0dee88785679d45170060b19a602
Jack Doan <jackdoan@rivian.com>
2022-09-30 13:22:52 -0500

    Fix ResetInputBuffer and ResetOutputBuffer on Darwin

--- serial_unix.go
+++ serial_unix.go
@@ -117,14 +117,6 @@ func (port *unixPort) Write(p []byte) (n int, err error) {
 	return
 }
 
-func (port *unixPort) ResetInputBuffer() error {
-	return unix.IoctlSetInt(port.handle, ioctlTcflsh, unix.TCIFLUSH)
-}
-
-func (port *unixPort) ResetOutputBuffer() error {
-	return unix.IoctlSetInt(port.handle, ioctlTcflsh, unix.TCOFLUSH)
-}
-
 func (port *unixPort) SetMode(mode *Mode) error {
 	settings, err := port.getTermSettings()
 	if err != nil {
